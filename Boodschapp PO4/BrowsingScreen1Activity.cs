76cf116e1ae4ce51763662558170e4857a104b71
Fabio <0948797@hr.nl>
2018-06-14 11:04:26 +0200

    j

--- Boodschapp PO4/BrowsingScreen1Activity.cs
+++ Boodschapp PO4/BrowsingScreen1Activity.cs
@@ -13,7 +13,7 @@ using System.Threading.Tasks;
 
 namespace Boodschapp_PO4
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = false)]
     public class BrowsingScreen1Activity : AppCompatActivity
     {
         RecyclerView                    mRecyclerView;
