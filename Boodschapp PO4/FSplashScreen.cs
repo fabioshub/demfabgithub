92bcdd5015576781c9cc5670eb96fd465f988410
Camphens <37102286+Camphens@users.noreply.github.com>
2018-06-17 10:29:08 +0200

    Added SplashScreen

---
+++ Boodschapp PO4/FSplashScreen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Boodschapp_PO4
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class FSplashScreen : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Splasher);
+            TheTransition();
+
+        }
+
+        async void TheTransition()
+        {
+            var intent = new Intent(this, typeof(BrowsingScreen1Activity));
+            await Task.Delay(4000);
+            StartActivity(intent);
+            OverridePendingTransition(Resource.Animation.abc_fade_in, Resource.Animation.abc_fade_out);
+        }
+    }
+}
